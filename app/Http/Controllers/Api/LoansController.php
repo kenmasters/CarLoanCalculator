d96f413d9dbc46ca8d6fbace85cca9fcbd20c78c
SV-MSI-0001\User <it.ops@springvalley.tech>
2022-12-08 04:43:45 +0800

    remove vue folder

--- app/Http/Controllers/Api/LoansController.php
+++ app/Http/Controllers/Api/LoansController.php
@@ -49,4 +49,50 @@ class LoansController extends Controller
 
         return response()->json($response);
     }
+
+    public function compounding(Request $request)
+    {
+        // validate that $principal must not be numeric and greater than zero.
+        // $request->validate([
+        //     'amount' => 'required|numeric|gt:0',
+        // ]);
+
+        // Formula
+        /**
+         * A = P * ((r * pow(1 + r, n)) / (pow(1+r, n) - 1))
+         */
+        // Payment Frequency:: Monthly
+        // Total Interest Due:
+        $principal = $request->input("principalValue");
+        $P = $principal; // Loan Amount:
+        $r = .06 / 12; // 6% is the Annual Interest Rate
+        $n = 5 * 12;
+        $A  = $P * (($r * pow(1+$r, $n)) / (pow(1+$r, $n) - 1)); // Periodic Payment (per month)
+        $PMT = $P * $r * $n;
+        $result_of_first_month_interest_payment = 10000 * $r;
+
+        $response = [
+            'periodicPayment' => number_format($A, 2),
+            'PMT' => $PMT,
+            'result_of_first_month_interest_payment' => $A - $result_of_first_month_interest_payment
+
+        ];
+        return response()->json($response);
+
+        // I = P x R x T
+        $interest = $principal * self::ANNUAL_INTEREST_RATE * self::LOAN_TERM_IN_YEARS;
+
+        $monthlyRepayment = ($principal + $interest) / (self::LOAN_TERM_IN_YEARS * self::MONTHS_IN_YEAR);
+
+        $response = [
+            "principal" => number_format($principal, 2, '.', ','),
+            "interest" => number_format($interest, 2, '.', ','),
+            "rate" => self::ANNUAL_INTEREST_RATE * 100,
+            "termInYear" => self::LOAN_TERM_IN_YEARS,
+            "termInMonth" => self::LOAN_TERM_IN_YEARS * self::MONTHS_IN_YEAR,
+            "monthlyRepayment" => number_format($monthlyRepayment, 2, '.', ','),
+        ];
+
+        return response()->json($response);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('calculate', [LoansController::class, "calculate"]);
+Route::post('compounding', [LoansController::class, "compounding"]);
